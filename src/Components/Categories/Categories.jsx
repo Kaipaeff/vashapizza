5076aa6a39c56f2215139658722428b1874845b1
Aslanbek <kaipaeff@gmail.com>
2023-03-06 18:44:09 +0300

    useState

--- src/Components/Categories/Categories.jsx
+++ src/Components/Categories/Categories.jsx
@@ -1,8 +1,8 @@
 function Categories() {
   return (
-    <div class="categories">
+    <div className="categories">
       <ul>
-        <li class="active">Все</li>
+        <li className="active">Все</li>
         <li>Мясная</li>
         <li>Куриная</li>
         <li>Грибная</li>
