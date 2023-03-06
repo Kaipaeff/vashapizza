2ab80550c30840ecf67a623bf9add5303bcf2576
Aslanbek <kaipaeff@gmail.com>
2023-03-06 17:56:03 +0300

    components too

---
+++ src/Components/Categories/Categories.jsx
@@ -0,0 +1,16 @@
+function Categories() {
+  return (
+    <div class="categories">
+      <ul>
+        <li class="active">Все</li>
+        <li>Мясная</li>
+        <li>Куриная</li>
+        <li>Грибная</li>
+        <li>Сырная</li>
+        <li>Овощная</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Categories
