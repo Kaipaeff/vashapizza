ecf8fb15f54a9e66c4264017179c0eeb1940a648
Aslanbek <kaipaeff@gmail.com>
2023-03-11 21:39:27 +0300

    #12 all done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
+/* eslint-disable import/no-cycle */
 import React from 'react';
-// eslint-disable-next-line import/no-extraneous-dependencies
 import { Routes, Route } from 'react-router-dom';
-// eslint-disable-next-line import/no-cycle
 import Header from './Components/Header/Header.jsx';
 import NotFoundBlock from './Components/NotFoundBlock/index.jsx';
-// eslint-disable-next-line import/no-cycle
 import Home from './Pages/Home.jsx';
 import Cart from './Pages/Cart.jsx';
 import './scss/app.scss';
+import { useSelector, useDispatch } from 'react-redux';
 
 export const SearchContext = React.createContext();
 
 function App() {
   const [searchValue, setSearchValue] = React.useState('');
+  // const filter = useSelector((state) => state.filter.value);
+  // const dispatch = useDispatch();
 
   return (
     <div className="wrapper">
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,9 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { configureStore } from '@reduxjs/toolkit';
+import filterReducer from './slices/filterSlice';
 
 export default configureStore({
-  reducer: {},
+  reducer: {
+    filter: filterReducer,
+  },
 });
